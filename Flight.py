6443c7790291439826461adb328b45c37548614a
waleedtarazi <waleedaltarazy@gmail.com>
2025-02-13 11:56:47 +0300

    Adding Flight class

---
+++ Flight.py
@@ -0,0 +1,9 @@
+import random
+class Flight():
+    def __init__(self, departure, destination):
+        self.departure = departure
+        self.destination = destination
+        self.id = random.random() 
+
+class Booking():
+    pass
